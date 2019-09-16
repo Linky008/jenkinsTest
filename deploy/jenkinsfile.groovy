26f4d46fdb5325d9983b41cec5bc475a53d5133f
ljzheng <ljzheng@homeinns.com>
2019-09-16 18:13:27 +0800

    111

--- deploy/jenkinsfile.groovy
+++ deploy/jenkinsfile.groovy
@@ -1,10 +1,10 @@
 #!groovy
 //电子房价牌
-library 'common'
+library 'CoreWeb'
 def map = [:]
-map.put("PROJECT_FOLDER","src/HomeInnsEBusiness.H5")
-map.put('PROJECT_NAME','HomeInnsEBusiness.H5')
-map.put('IMAGE_NAME','bthgyouxuan')
+map.put("PROJECT_FOLDER","src/CoreWeb")
+map.put('PROJECT_NAME','CoreWeb')
+//map.put('IMAGE_NAME','bthgyouxuan')
 map.put('GIT_URL','https://github.com/Linky008/jenkinsTest')
 map.put('SIT_PREVIEW_URL','http://192.168.222.128:31025')
 map.put('UAT_PREVIEW_URL','http://192.168.222.128:31025')
