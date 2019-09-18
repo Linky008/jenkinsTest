21a096c70c310ab9f0a1d0c96624caed923296e2
ljzheng <ljzheng@homeinns.com>
2019-09-18 11:03:31 +0800

    111

--- deploy/jenkinsfile.groovy
+++ deploy/jenkinsfile.groovy
@@ -1,18 +1,21 @@
 #!groovy
-//电子房价牌
-//library 'CoreWeb'
-def map = [:]
-map.put("PROJECT_FOLDER","src/CoreWeb")
-map.put('PROJECT_NAME','CoreWeb')
-//map.put('IMAGE_NAME','bthgyouxuan')
-map.put('GIT_URL','https://github.com/Linky008/jenkinsTest')
-map.put('SIT_PREVIEW_URL','http://192.168.222.128:31025')
-map.put('UAT_PREVIEW_URL','http://192.168.222.128:31025')
-map.put('PROD_PREVIEW_URL','http://192.168.222.128:31025')
-
-map.put('DEPLOY_ENV','sit\nprod')
-map.put('DEPLOY_VERSION','1.0')
-map.put('REGISTRY_URL','http://192.168.222.1')
-
-//netCoreDeployToK8s(map)
-
+pipeline {
+    agent any
+    stages {
+        stage('Git Checkout') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '10001', url: 'https://github.com/Linky008/jenkinsTest']]])
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying'
+            }
+        }
+    }
+}
